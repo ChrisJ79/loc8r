a893cae384d5817b93d43d7643e85317ca544052
Chris Johnson <csjohnson79@icloud.com>
2016-12-07 20:55:33 -0700

    updated

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var main = require('../controllers/main.js');
+// var main = require('../controllers/main.js');
+var ctrlLocations = require('../controllers/locations.js');
+var ctrlOthers = require('../controllers/others.js');
 
-var homepageController = function (req, res) {
-  res.render('index', { title: 'Express' });
-};
+
+// var homepageController = function (req, res) {
+//   res.render('index', { title: 'Express' });
+// };
+
+/* GET LOCATIONS pages. */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* GET OTHER pages. */
+router.get('/about', ctrlOthers.about);
+
+
+
 
 /* GET home page. */
-router.get('/', main.index);
+// router.get('/', main.index);
 
 
 module.exports = router;
